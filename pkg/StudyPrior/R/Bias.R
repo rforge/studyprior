aae6149d76ea40e22ff0b76c6b250e3562ff78b5
igrave <igrave@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-03-23 14:01:41 +0000

    Fix copy paste errors and add proprer parallel support for bias calculations
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/studyprior@37 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/StudyPrior/R/Bias.R
+++ pkg/StudyPrior/R/Bias.R
@@ -11,7 +11,7 @@
 #' @export
 #'
 #' @examples
-calc.bias <- function(prior, prob.range=c(.5,1), length=20, n.binom=30, posterior){
+calc.bias <- function(prior, prob.range=c(.5,1), length=20, n.binom=30, posterior, mc.cores=1){
 
   P <- seq(prob.range[1],prob.range[2],len=length)
   if(missing(posterior)){
@@ -38,14 +38,17 @@ calc.bias <- function(prior, prob.range=c(.5,1), length=20, n.binom=30, posterio
       }
     })
   } else if(!missing(posterior)){
-    MSE.for.x <- parallel::mclapply(posterior, function(post){
-      Ep <- function(p, true.p) post(p) *p
+    Bias.for.x <- parallel::mclapply(posterior, function(post){
+      print("Biasing!")
+      print(post)
+      Ep <-  adaptIntegrate(function(p) post(p)*p, 0,1, maxEval=1e4)$integral
 
-      return(sapply(P, function(true.p){
-        adaptIntegrate(Ep,0,1, true.p=true.p, maxEval=1e4)$integral - true.p})
+      return(sapply(P, function(true.p){ Ep - true.p})
       )
       }, mc.cores = mc.cores)
   }
+
+  Bias.for.x <- matrix(unlist(Bias.for.x), nrow=length)
 
   sapply(seq_along(P), function(i){
    sum(Bias.for.x[i,] * dbinom(0:n.binom, size=n.binom, prob=P[i]))})
