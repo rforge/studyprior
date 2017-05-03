72866b9dd1efd39439a87c3d833496a4586321db
igrave <igrave@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-05-03 14:40:50 +0000

    Use linear interpolation. Set a higher default length to make sure this approximation is ok
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/studyprior@45 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/StudyPrior/R/binom.PP.FB.MC.COR.R
+++ pkg/StudyPrior/R/binom.PP.FB.MC.COR.R
@@ -18,7 +18,7 @@
 #' @importFrom mvtnorm rmvnorm
 #'
 #' @examples
-binom.PP.FB.MC.COR <- function(x, n, verbose=FALSE, length=30, d.prior.cor=0, p.prior.a=1, p.prior.b=1, mc.cores=1, samples=10000, focus ){
+binom.PP.FB.MC.COR <- function(x, n, verbose=FALSE, length=200, d.prior.cor=0, p.prior.a=1, p.prior.b=1, mc.cores=1, samples=10000, focus ){
   n.hist <- length(x)
 
   #Where to calculate density at
@@ -44,7 +44,19 @@ binom.PP.FB.MC.COR <- function(x, n, verbose=FALSE, length=30, d.prior.cor=0, p.
       1,eval.f))
   },mc.cores=mc.cores)
 
-  dens2 <- density(rep(p,unlist(dens)*10000),from=0,to=1, bw=.02)
+#
+#
+#   a <- smooth.spline(p, dens)
+#   dens2 <- density(rep(p,unlist(dens)*10000),from=0,to=1, bw=.01,)
+#   opts <- optimr::optimr(c(2,3), function(Q) sum((dens2$y - dbeta(dens2$x, Q[1],Q[2]))^2),
+#                          lower=c(1,1), upper=c(100,100), method="L-BFGS-B")
+# dd <- density(rep(a$x,a$y*10000), from=0, to=1)
+# lines(dd)
+# points(a$x,a$y)
+# plot(a$x,a$y, xlim=c(0,.5), ylim=c(0,1e-5))
+#
+# dens2$y[dens2$y<1e-15] <- dbeta(dens2$x[dens2$y<1e-15],opts$par[1],opts$par[2])
+
   # f <- splinefun(smooth.spline(x=p, y=pmax(dens,0)))
 
 
@@ -53,8 +65,10 @@ binom.PP.FB.MC.COR <- function(x, n, verbose=FALSE, length=30, d.prior.cor=0, p.
   # f <- splinefun(x=p, y=unlist(dens)/k)
 
   # g <- function(p,X) ifelse(0<=p&p<=1, f(p),0)
+#
+  # g <- approxfun(dens2, yleft=0, yright=0)
+  g <- approxfun(c(0,p,1), c(0,unlist(dens),0))
 
-  g <- approxfun(dens2, yleft=0, yright=0)
   formals(g) <- alist(v=,...=)
 
   return(g)
