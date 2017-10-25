d0a358d67e160d9af77e8efb081902c7f31400b8
igrave <igrave@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-10-25 14:01:14 +0000

    optimise sig.matrix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/studyprior@68 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/StudyPrior/R/sig.matrix.R
+++ pkg/StudyPrior/R/sig.matrix.R
@@ -45,13 +45,15 @@ sig.matrix <- function(n.control, n.treatment, level=0.975, prior, posterior, tr
             function(p) eval.mixture.prior(p, post.list)
         }
         } else if(use.posterior) {
-            if(inherits(posterior[[1]],"mixture.prior")) function(p) eval.mixture.prior(p, posterior[[Xs+1]])
+            if(inherits(posterior[[1]],"mixture.prior"))  approxfun(seq(0,1,len=200),
+                                                                    eval.mixture.prior(seq(0,1,len=200),
+                                                                                       posterior[[Xs+1]]))
           else posterior[[Xs+1]]
           }
       
     ZZ <-unlist(
       lapply(check.xt, function(xT){
-      # print(paste0('.',xT))
+       print(paste0('.',xT))
       res <-  try({
         # if(debug ) browser()
 
